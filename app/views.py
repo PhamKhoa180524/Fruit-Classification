f5b6fa0e8a50a6f5b5e5dde49b401df7adac7d53
dangkhoa <phamkhoa180524@gmail.com>
2025-05-27 21:52:11 +0700

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,32 @@
+from flask import render_template, request
+from flask import redirect, url_for
+import os
+from PIL import Image
+from app.utils import pipeline_model
+
+UPLOAD_FLODER = 'static/uploads'
+
+
+def getwidth(path):
+    img = Image.open(path)
+    size = img.size # width and height
+    aspect = size[0]/size[1] # width / height
+    w = 300 * aspect
+    return int(w)
+
+def fresh():
+
+    if request.method == "POST":
+        f = request.files['image']
+        filename=  f.filename
+        path = os.path.join(UPLOAD_FLODER,filename)
+        f.save(path)
+        w = getwidth(path)
+        # prediction (pass to pipeline model)
+        predict,score=pipeline_model(path,filename,color='bgr')
+
+
+        return render_template('fresh.html',fileupload=True,img_name=filename, w=w,predict=predict,score=score)
+
+
+    return render_template('fresh.html',fileupload=False,img_name="")
